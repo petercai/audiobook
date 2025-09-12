1193c304bd3b2e0ecdb8efe418ecca1fd1b432d6
Peter Cai <petercaica@hotmail.com>
2025-09-12 11:04:50 -0400

    set pip proxy

--- run.cmd
+++ run.cmd
@@ -1,4 +1,6 @@
-python app.py --share
+python app.py
 
+pip install -r requirements.txt --proxy http://webproxy.bns:8080 --trusted-host pypi.org --trusted-host files.pythonhosted.org
+pip install -r requirements.txt --proxy http://localhost:9000 --trusted-host pypi.org --trusted-host files.pythonhosted.org
 
 python app.py --headless --ebook ebooks/god-c1.txt --language zh-cn --voice zho/adult/male/yunyang_24000.wav --output_dir audiobook_output
