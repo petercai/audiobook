21c5b449ecc8d6dbb139ba7de6688b21af385990
Peter Cai <petercaica@hotmail.com>
2025-09-13 13:52:10 -0400

    env

--- run.cmd
+++ run.cmd
@@ -3,4 +3,11 @@ python app.py
 pip install -r requirements.txt --proxy http://webproxy.bns:8080 --trusted-host pypi.org --trusted-host files.pythonhosted.org
 pip install -r requirements.txt --proxy http://localhost:9000 --trusted-host pypi.org --trusted-host files.pythonhosted.org
 
-python app.py --headless --ebook ebooks/god-c1.txt --language zh-cn --voice zho/adult/male/yunyang_24000.wav --output_dir audiobook_output
+python app.py --headless --ebook ebooks/god-c1.txt --language zh-cn --voice zho/adult/male/yunxi_24000.wav --output_dir tmp
+python app.py --headless --ebook ebooks/god-c12.epub --language zh-cn --voice zho/adult/male/yunxi_24000.wav --output_dir tmp
+
+
+python app.py --headless --ebook ebooks/god-c1.txt --language zho --voice voices/zho/adult/male/yunxi_24000.wav --output_dir tmp
+
+python app.py --headless --ebook ebooks/god-c1.txt --device mps --tts_engine xtts --language zho --voice voices/zho/adult/male/yunxi_24000.wav --output_dir tmp
+python app.py --headless --ebook ebooks/god-c1.txt --device mps --tts_engine bark --language zho --voice voices/zho/adult/male/yunxi_24000.wav --output_dir tmp
