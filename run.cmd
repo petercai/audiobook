7e0c6174c1b628aecbad35f4d15ac5d8604ecc4e
Peter Cai <petercaica@hotmail.com>
2025-09-14 23:12:58 -0400

    run.cmd

--- run.cmd
+++ run.cmd
@@ -1,3 +1,32 @@
+@REM To configure **Poetry** to always create the virtual environment in the current folder as `.venv`,
+poetry config virtualenvs.in-project true --local
+
+@REM You can specify a full path to the Python interpreter:
+poetry env use /full/path/to/python
+
+
+@REM which tells Poetry to use the currently active Python environment
+poetry config virtualenvs.prefer-active-python true --local
+
+
+@REM If you want to revert to the default system Python behavior, you can run:
+poetry env use system
+
+
+@REM ### 1. **List All Poetry Virtual Environments**
+poetry env list
+
+@REM ### 2. **Show Full Path of the Active Virtual Environment**
+poetry env info
+@REM This will display details like:
+@REM - Path to the virtual environment
+@REM - Python version used
+@REM - Whether the environment is currently activated
+
+@REM ### 3. **Get Only the Virtual Environment Path**
+poetry env info --path
+
+
 python app.py --share
 
 
