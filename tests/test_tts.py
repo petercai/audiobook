2a7453a4bcafdb3217fa89c7199ad8cfcad56b93
Peter Cai <petercaica@hotmail.com>
2025-09-30 14:00:00 -0400

    pytest

--- tests/test_tts.py
+++ tests/test_tts.py
@@ -1,8 +1,27 @@
+from math import log
 import torch
 from TTS.api import TTS
 import pytest
 import logging
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# File handler
+file_handler = logging.FileHandler("test_tts.log")
+file_handler.setLevel(logging.INFO)
+
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+# Add handlers to logger
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 
 @pytest.fixture(scope="module")
@@ -11,10 +30,12 @@ def device():
 
 @pytest.fixture(scope="module")
 def tts(device):
-  return TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+  _tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+  return _tts
 
 def test_list_models():
   models = TTS().list_models()
+  logger.info("models")
   for model in models:
     logger.info(model)
   assert isinstance(models, list)
@@ -22,15 +43,17 @@ def test_list_models():
 
 def test_list_speakers(tts):
   speakers = tts.speakers
+  logger.info("speakers of model " + tts.model_name)
   for speaker in speakers:
-    print(speaker)
+    logger.info(speaker)
   assert isinstance(speakers, list)
   assert len(speakers) > 0
 
 def test_list_languages(tts):
   languages = tts.languages
+  logger.info("languages of model " + tts.model_name)
   for lang in languages:
-    print(lang)
+    logger.info(lang)
   assert isinstance(languages, list)
   assert "en" in languages
 
