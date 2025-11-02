7b70e78bb60b06ad409de41ff03bafc1e2b8d26f
Peter Cai <petercaica@hotmail.com>
2025-11-02 01:27:14 -0400

    test epub

--- tests/test_epub.py
+++ tests/test_epub.py
@@ -23,17 +23,21 @@ def session_context(tmp_path):
     context = SessionContextMock({"session": session_id})
     session = context.get_session(session_id)
 
-    # Create necessary directories
-    process_dir = os.path.join(tmp_path,  "test_process")
-    os.makedirs(process_dir, exist_ok=True)
-    session['process_dir'] = str(process_dir)
+    # # Create necessary directories
+    # process_dir = os.path.join(tmp_path,  "test_process")
+    # os.makedirs(process_dir, exist_ok=True)
+    # session['process_dir'] = str(process_dir)
 
     return context, session_id, session
 
 def test_convert2epub(session_context, ebook_path, tmp_path):
     """Test successful conversion of a .txt file to .epub."""
     context, session_id, session = session_context
-    session = context.get_session(session_id)
+    # session = context.get_session(session_id)
+    # Create necessary directories
+    process_dir = os.path.join(tmp_path,  "test_process")
+    os.makedirs(process_dir, exist_ok=True)
+    session['process_dir'] = str(process_dir)
 
     input_file = os.path.join(ebook_path, "god-c12-short.epub")
 
@@ -46,7 +50,10 @@ def test_convert2epub(session_context, ebook_path, tmp_path):
 def test_process_epub(session_context, ebook_path, tmp_path):
     """Test successful processing of an EPUB file."""
     context, session_id, session = session_context
-    # session = context.get_session(session_id)
+    # Create necessary directories
+    process_dir = os.path.join(tmp_path,  "test_process")
+    os.makedirs(process_dir, exist_ok=True)
+    session['process_dir'] = str(process_dir)
 
     # Setup arguments for EBookProcessor
     args = {
@@ -93,10 +100,13 @@ def test_process_epub(session_context, ebook_path, tmp_path):
     assert "Audiobook(s)" in status
     assert os.path.exists(session['audiobook'])
     
-def test_process_epub_chapters(session_context, ebook_path, tmp_path):
+def test_process_epub_chapters_cn(session_context, ebook_path, tmp_path):
     """Test successful processing of an EPUB file."""
     context, session_id, session = session_context
-    # session = context.get_session(session_id)
+    # Create necessary directories
+    process_dir = os.path.join(tmp_path,  "test_god_c12")
+    os.makedirs(process_dir, exist_ok=True)
+    session['process_dir'] = str(process_dir)
 
     # Setup arguments for EBookProcessor
     args = {
@@ -109,6 +119,69 @@ def test_process_epub_chapters(session_context, ebook_path, tmp_path):
         "device": "cpu",
         "language": "zho",
         "language_iso1": "zh",
+        "tts_engine": TTS_ENGINES['XTTSv2'],
+        "output_format": "m4b",
+        "custom_model": None,
+        "fine_tuned": "internal",
+        "voice": None,
+        "voice_dir": os.path.join(voices_dir, '__sessions', "test_voice"),
+        "speaker_wav": os.path.join(voices_dir, "zho", "adult", "male", "yunjian_24000.wav"),
+        "temperature": 0.75,
+        "length_penalty": 1.0,
+        "num_beams": 5,
+        "repetition_penalty": 1.0,
+        "top_k": 50,
+        "top_p": 0.95,
+        "speed": 1.0,
+        "enable_text_splitting": True,
+        "text_temp": 0.7,
+        "waveform_temp": 0.7,
+        "audiobooks_dir": tmp_path,
+        "output_split": "by-chapter",
+        "output_split_hours": 1,
+        "is_gui_process": False,
+        "script_mode": "native"
+    }
+    # update session with args
+    session.update(args)
+    os.makedirs(session['chapters_dir'], exist_ok=True)
+    os.makedirs(session['chapters_dir_sentences'], exist_ok=True)
+
+    # Instantiate EBookProcessor
+    ebook_processor = EBookProcessor()
+    session["epub_path"] = session["ebook"]
+    epubBook = epub.read_epub(session["ebook"], {"ignore_ncx": True})
+    basename = os.path.basename(session["ebook"])
+    name_splits = os.path.splitext(basename)
+    session["filename_noext"] = name_splits[0]
+
+    # Process the EPUB
+    status, success = ebook_processor.process_epub_chapters(epubBook, session)
+
+    # Assertions
+    assert success is True
+    print(session['audiobook'])
+    assert os.path.exists(session['audiobook'])
+
+def test_process_epub_chapters_en(session_context, ebook_path, tmp_path):
+    """Test successful processing of an EPUB file."""
+    context, session_id, session = session_context
+    # Create necessary directories
+    process_dir = os.path.join(tmp_path,  "test_UnravelMe-c12")
+    os.makedirs(process_dir, exist_ok=True)
+    session['process_dir'] = str(process_dir)
+
+    # Setup arguments for EBookProcessor
+    args = {
+        "session": session_id,
+        'cancellation_requested': False,
+        "ebook": os.path.join(ebook_path, "UnravelMe-c12.epub"),
+        "chapters_dir": os.path.join(session['process_dir'], "chapters"),
+        "chapters_dir_sentences": os.path.join(session['process_dir'], "chapters", "sentences"),
+        "ebook_list": None,
+        "device": "cpu",
+        "language": "eng",
+        "language_iso1": "en",
         "tts_engine": TTS_ENGINES['XTTSv2'],
         "output_format": "m4b",
         "custom_model": None,
