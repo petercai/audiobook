f3c1cdcf70f09e9aea5b3fde205c2e6c67cc9b8a
Peter Cai <petercaica@hotmail.com>
2025-12-30 22:51:42 -0500

    batch.sh

---
+++ batch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+python app.py
+ebook_path="$1"
+book_name="${ebook_path##*/}"
+book_name="${book_name%.*}"
+
+python app.py --headless \
+  --session "$book_name" \
+  --ebook "$ebook_path" \
+  --device cpu \
+  --add_toc_title \
+  --language en \
+  --tts_engine xtts \
+  --fine_tuned DermotCrowley \
+  --output_format mp4
