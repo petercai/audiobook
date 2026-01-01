32e9c8ce602d8344526b6e49dbe62bc9022eb1a7
Peter Cai <petercaica@hotmail.com>
2026-01-01 16:02:36 -0500

    en1 & en2

--- tests/test_epub.py
+++ tests/test_epub.py
@@ -251,7 +251,44 @@ def test_process_epub_chapters_cn(session_context, ebook_path, tmp_path):
     print(session['audiobook'])
     assert os.path.exists(session['audiobook'])
 
-def test_process_epub_by_chapters_en(session_context, ebook_path, tmp_path):
+def test_process_epub_by_chapters_en1(session_context, ebook_path, tmp_path):
+    """Test successful processing of an EPUB file."""
+    context, session_id, session = session_context
+    args = {
+        "ebook": os.path.join(ebook_path, "English-1.epub"),
+        "ebook_list": None,
+        "device": "cpu",
+        "language": "eng",
+        "language_iso1": "en",
+        "tts_engine": TTS_ENGINES['XTTSv2'],
+        "output_format": "mp4",
+        "offline_mode": False,
+    }
+    # update session with args
+    session.update(args)
+    
+    # Create necessary directories
+    func_name = inspect.currentframe().f_code.co_name
+    set_process_dir(session, func_name)
+    session['epub_path'] = session['ebook']
+    basename = os.path.basename(session["ebook"])
+    name_splits = os.path.splitext(basename)
+    session["filename_noext"] = name_splits[0]
+
+    # Instantiate EBookProcessor
+    from lib.ebook_processor import EBookProcessor
+    ebook_processor = EBookProcessor()
+    epubBook = epub.read_epub(session["ebook"], {"ignore_ncx": True})
+
+    # Process the EPUB
+    status, success = ebook_processor.process_epub_by_chapters(epubBook, session)
+
+    # Assertions
+    assert success is True
+    print(session['audiobook'])
+    assert os.path.exists(session['audiobook'])
+    
+def test_process_epub_by_chapters_en2(session_context, ebook_path, tmp_path):
     """Test successful processing of an EPUB file."""
     context, session_id, session = session_context
     args = {
@@ -262,6 +299,7 @@ def test_process_epub_by_chapters_en(session_context, ebook_path, tmp_path):
         "language_iso1": "en",
         "tts_engine": TTS_ENGINES['XTTSv2'],
         "output_format": "mp4",
+        "offline_mode": False,
     }
     # update session with args
     session.update(args)
