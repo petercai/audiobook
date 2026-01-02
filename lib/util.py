655deb388b019d49aa08e96511b09a57a2c81102
Peter Cai <petercaica@hotmail.com>
2026-01-02 00:50:22 -0500

    number_width

--- lib/util.py
+++ lib/util.py
@@ -19,7 +19,8 @@ class util:
         chapter_count = len(chapters_in_book)
         number_width = max(1, len(str(chapter_count)))
         for chapter_index, chapter_sentences in enumerate(chapters_in_book, 1):
-            chapter_filename = f"chapter_{chapter_index:0{number_width}d}.txt"
+            # chapter_filename = f"chapter_{chapter_index:0{number_width}d}.txt"
+            chapter_filename = f"chapter_{chapter_index}.txt"
             chapter_path = os.path.join(chapter_dir, chapter_filename)
             with open(chapter_path, "w", encoding="utf-8") as chapter_file:
                 chapter_file.write("\n".join(chapter_sentences))
